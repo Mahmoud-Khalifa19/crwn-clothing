6d62665bdbb9e09ee5d2dae01cf92379e76e5a56
mahmoud khalifa <mahmoudbkhalifa@gmail.com>
2020-05-21 02:42:49 +0100

    Updates

---
+++ precache-manifest.b56ae4772b1e1e42b4f0a244e41bbeaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0287b70a2d2d8225d16a203223021545",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "20b1351c0baca5732d78",
+    "url": "/crwn-clothing/static/css/main.d13fbdf1.chunk.css"
+  },
+  {
+    "revision": "aa714313500494f07032",
+    "url": "/crwn-clothing/static/js/2.36e72387.chunk.js"
+  },
+  {
+    "revision": "20b1351c0baca5732d78",
+    "url": "/crwn-clothing/static/js/main.2b74a52c.chunk.js"
+  },
+  {
+    "revision": "9f870041e6fb0a76314f",
+    "url": "/crwn-clothing/static/js/runtime~main.2a0a97f4.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
